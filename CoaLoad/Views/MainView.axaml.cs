dc7ba372738ca3495d682a77185558ef43d9d954
Mohammad Mahdi Mohammadi <mmm.6831.allah@gmail.com>
2025-01-06 21:45:44 +0330

    UI Improvements.

--- CoaLoad/Views/MainView.axaml.cs
+++ CoaLoad/Views/MainView.axaml.cs
@@ -1,7 +1,8 @@
 using System;
 using Avalonia.Controls;
-using Avalonia.Controls.Notifications;
+using Avalonia.Controls.Primitives;
 using Avalonia.Interactivity;
+using Avalonia.Media;
 
 namespace CoaLoad.Views;
 
@@ -15,6 +16,54 @@ public partial class MainView : UserControl
     private void DownloadButtonClicked(object? sender, RoutedEventArgs e)
     {
         Console.WriteLine("Download button clicked");
+        
     }
     
+
+    private void AutoButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (AutoButton.IsChecked == false)
+        {
+            AutoButton.IsChecked = true; // Prevent deselecting the button
+            return;
+        }
+
+        MuteButton.IsChecked = false;
+        AudioButton.IsChecked = false;
+        AutoIcon.Fill = new SolidColorBrush(Colors.Black);
+        AudioIcon.Fill = new SolidColorBrush(Colors.White);
+        MuteIcon.Fill = new SolidColorBrush(Colors.White);
+    }
+
+    private void AudioButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (AudioButton.IsChecked == false)
+        {
+            AudioButton.IsChecked = true; // Prevent deselecting the button
+            return;
+        }
+
+        AutoButton.IsChecked = false;
+        MuteButton.IsChecked = false;
+        AutoIcon.Fill = new SolidColorBrush(Colors.White);
+        AudioIcon.Fill = new SolidColorBrush(Colors.Black);
+        MuteIcon.Fill = new SolidColorBrush(Colors.White);
+    }
+
+    private void MuteButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (MuteButton.IsChecked == false)
+        {
+            MuteButton.IsChecked = true; // Prevent deselecting the button
+            return;
+        }
+
+        AutoButton.IsChecked = false;
+        AudioButton.IsChecked = false;
+        AutoIcon.Fill = new SolidColorBrush(Colors.White);
+        AudioIcon.Fill = new SolidColorBrush(Colors.White);
+        MuteIcon.Fill = new SolidColorBrush(Colors.Black);
+    }
+
+
 }
